0fa760885fa90b0e58cb6cad718275dc22d73af8
anuraag <anuraag.warudkar@gmail.com>
2024-03-18 12:31:42 -0400

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <>
-    <div className="h-screen flex justify-center items-center">
-    CLARITY
-    </div>
+      <div className="flex h-screen items-center justify-center">
+        CLARITY - test deploy
+      </div>
     </>
   );
 }
